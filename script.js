f928cae0c7493f08304ef1e38e4c12f846602f26
michiru <kirittoes@gmail.com>
2022-07-12 00:50:20 +0100

    Add correct grid and makeCard() function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = []
 
 let mainContent = document.querySelector(".mainContent")
+let main = document.querySelector(".main")
 let btnAdd = document.querySelector(".btnAdd")
 let btnSubmit = document.querySelector(".btnSubmit")
 let form = document.querySelector(".form")
@@ -25,8 +26,14 @@ function addBookToLibrary(newBook) {
     myLibrary.push(newBook)
 }  
 
-for (let i=0; i < myLibrary.length; i++) {
-    mainContent.textContent += "<li>" + myLibrary[i] + "</li>"
+book.prototype.makeCard = function () {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = myLibrary[i]
+        let cardText = document.createElement("div")
+        cardText.textContent = card
+        main.appendChild(cardText)
+    }
 }
+
 
 
