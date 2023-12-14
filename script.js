791c180514195257ce6fd5e02bf7a9937a904042
michiru <kirittoes@gmail.com>
2023-12-14 12:35:05 +0000

    Move the code that display book array items inside a function

--- script.js
+++ script.js
@@ -23,26 +23,29 @@ addBookToLibrary(warDoctor)
 const goingInfinite = new Book("Going Infinite: The Rise and Fall of a New Tycoon", "Michael Lews", 267, "read")
 addBookToLibrary(goingInfinite)
 
-for (let i = 0; i < myLibrary.length; i++) {
-    const bookCard = document.createElement("div")
-    bookCard.setAttribute("id", `bookCard`)
-    const title = document.createElement("div")
-    title.setAttribute("id", `title`)
-    const author = document.createElement("div")
-    author.setAttribute("id", `author`)
-    const pages = document.createElement("div")
-    pages.setAttribute("id", `pages`)
-    const readStatus = document.createElement("div")
-    readStatus.setAttribute("id", `readStatus`)
+function createCard() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookCard = document.createElement("div")
+        bookCard.setAttribute("id", `bookCard`)
+        const title = document.createElement("div")
+        title.setAttribute("id", `title`)
+        const author = document.createElement("div")
+        author.setAttribute("id", `author`)
+        const pages = document.createElement("div")
+        pages.setAttribute("id", `pages`)
+        const readStatus = document.createElement("div")
+        readStatus.setAttribute("id", `readStatus`)
 
-    gridContainer.append(bookCard)
-    bookCard.append(title)
-    bookCard.append(author)
-    bookCard.append(pages)
-    bookCard.append(readStatus)
+        gridContainer.append(bookCard)
+        bookCard.append(title)
+        bookCard.append(author)
+        bookCard.append(pages)
+        bookCard.append(readStatus)
 
-    title.textContent = `Title: ${myLibrary[i].title}`
-    author.textContent = `Author: ${myLibrary[i].author}`
-    pages.textContent = `Pages: ${myLibrary[i].pages}`
-    readStatus.textContent = `Read: ${myLibrary[i].readStatus}`
+        title.textContent = `Title: ${myLibrary[i].title}`
+        author.textContent = `Author: ${myLibrary[i].author}`
+        pages.textContent = `Pages: ${myLibrary[i].pages}`
+        readStatus.textContent = `Read: ${myLibrary[i].readStatus}`
+    }
 }
+createCard()
